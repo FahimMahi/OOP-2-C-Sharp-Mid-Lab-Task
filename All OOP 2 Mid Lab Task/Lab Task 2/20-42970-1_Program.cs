28330c35fd2033be1d8f460a8bbf3fcace4eacb5
Fahim Mahmud Bhuiyan (Mahi) <118056330+FahimMahi@users.noreply.github.com>
2023-04-02 01:44:26 +0600

    Add files via upload

---
+++ All OOP 2 Mid Lab Task/Lab Task 2/20-42970-1_Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your marks: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            if (x <= 100)
+            {
+                switch (x / 5)
+                {
+                    case 20:
+                        Console.WriteLine("A+");
+                        break;
+                    case 19:
+                        Console.WriteLine("A+");
+                        break;
+                    case 18:
+                        Console.WriteLine("A+");
+                        break;
+                    case 17:
+                        Console.WriteLine("A");
+                        break;
+                    case 16:
+                        Console.WriteLine("B+");
+                        break;
+                    case 15:
+                        Console.WriteLine("B");
+                        break;
+                    case 14:
+                        Console.WriteLine("C+");
+                        break;
+                    case 13:
+                        Console.WriteLine("C");
+                        break;
+                    case 12:
+                        Console.WriteLine("D+");
+                        break;
+                    case 11:
+                        Console.WriteLine("D");
+                        break;
+                    case 10:
+                        Console.WriteLine("D");
+                        break;
+                    default:
+                        Console.WriteLine("F");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Number");
+            }
+        }
+
+    }
+}
