56b87ac39fe8be4282f6ba1ca841aa91fdd746f2
Fahim Mahmud Bhuiyan (Mahi) <118056330+FahimMahi@users.noreply.github.com>
2023-04-02 01:58:29 +0600

    Add files via upload

---
+++ All OOP 2 Mid Lab Task/Lab Task 4/20-42970-1_Account.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTask4
+{
+    class Account
+    {
+        private string accName;
+        private string acid;
+        private int balance;
+
+        public Account()
+        {
+            //Empty Constructor
+        }
+
+        public Account(string aName, string aID, int Balance)
+        {
+            this.AccName = aName;
+            this.AccID = aID;
+            this.AccBalance = Balance;
+        }
+
+        public string AccName
+        {
+            get
+            {
+                return accName;
+            }
+            set
+            {
+                accName = value;
+            }
+        }
+
+        public string AccID
+        {
+            get
+            {
+                return acid;
+            }
+            set
+            {
+                acid = value;
+            }
+        }
+
+        public int AccBalance
+        {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                balance = value;
+            }
+        }
+
+        public void Deposit(int amount)
+        {
+            AccBalance = AccBalance + amount;
+        }
+
+        public void Withdraw(int amount)
+        {
+            AccBalance = AccBalance - amount;
+        }
+    }
+}
---
+++ All OOP 2 Mid Lab Task/Lab Task 4/20-42970-1_Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account obj = new Account("Fahim Mahmud Bhuiyan", "20-42970-1", 1000);
+            Console.WriteLine("Account's Owner Name: " + obj.AccName);
+            Console.WriteLine("Account's Owner ID: " + obj.AccID);
+            Console.WriteLine("Account's Owner Balance: " + obj.AccBalance);
+            obj.Withdraw(500);
+            Console.WriteLine("After Withdrawing Account's Owner Balance: " + obj.AccBalance);
+            obj.Deposit(420);
+            Console.WriteLine("After Depositing Account's Owner Balance: " + obj.AccBalance);
+        }
+    }
+}
