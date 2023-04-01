606604dd0833a613d61eb330258949d26b21205a
Fahim Mahmud Bhuiyan (Mahi) <118056330+FahimMahi@users.noreply.github.com>
2023-04-02 01:56:11 +0600

    Add files via upload

---
+++ All OOP 2 Mid Lab Task/Lab Task 3/20-42970-1_Lab-Task 3(1).cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_2_Lab_task_2
+{
+    class Mammals
+    {
+
+        public void m()
+        {
+            Console.WriteLine("I am mammal");
+        }
+    }
+
+    class MarineAnimals : Mammals
+    {
+
+        public void animal()
+        {
+            Console.WriteLine("I am a marine animal");
+        }
+    }
+
+    class BlueWhale : MarineAnimals
+    {
+        public void categories()
+        {
+            Console.WriteLine("I belong to both categories: Mammals as well as Marine Animals");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlueWhale obj = new BlueWhale();
+            obj.m();
+            obj.animal();
+            obj.categories();
+        }
+    }
+}
---
+++ All OOP 2 Mid Lab Task/Lab Task 3/20-42970-1_Lab-Task 3(2).cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Shape{
+        public void sha()
+        {
+            Console.WriteLine("This is a shape");
+        }
+    }
+    class Polygon : Shape
+    {
+        public void pol()
+        {
+            Console.WriteLine("Polygon is a shape");
+        }
+    }
+    class Rectangle : Polygon
+    {
+        public void Rec(){
+            Console.WriteLine("Rectangle is a polygon");
+        }
+    }
+    class Triangle : Polygon
+    {
+        public void tri(){
+            Console.WriteLine("Triangle is a polygon");
+        }
+    }
+    class Square : Rectangle
+    {
+        public void squ(){
+            Console.WriteLine("Square is a rectangle");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle obj = new Rectangle();
+            obj.sha();
+            obj.pol();
+            obj.Rec();
+            Triangle obj1 = new Triangle();
+            obj1.tri();
+            Square obj2 = new Square();
+            obj2.squ();
+        }
+    }
+}
